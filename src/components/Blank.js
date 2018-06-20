3cc1ea0561bc03ec4db1b8c540b77ece8cd88fee
mcabrex <mrcabardo@gmail.com>
2018-06-20 17:22:39 -0400

    first commit

---
+++ src/components/Blank.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Blank extends Component {
+  constructor(props){
+      super();
+      this.state = {
+
+      }
+    }
+
+    render() {
+      return (
+        <div>
+        </div>
+      );
+    }
+}
